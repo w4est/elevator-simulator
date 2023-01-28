d0193464fdd729bc1246414ba83af47a13b5a5b5
FMahamud <91301445+FMahamud@users.noreply.github.com>
2023-01-27 19:08:29 -0500

    Added 'elevator' package and classes

---
+++ iteration1/src/elevator/ElevatorSubsystem.java
@@ -0,0 +1,18 @@
+package elevator;
+
+import java.util.ArrayList;
+
+public class ElevatorSubsystem implements Runnable{
+	
+	ArrayList<Elevator> elevators;
+	
+	public ElevatorSubsystem() {
+		elevators = new ArrayList<>();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
