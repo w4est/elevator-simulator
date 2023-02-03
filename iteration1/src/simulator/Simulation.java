e3f570b0225d313bb5f9d2c4428966ae29a741eb
William Forrest <wd4est@gmail.com>
2023-02-03 18:44:04 -0500

    Added tests

--- iteration1/src/simulator/Simulation.java
+++ iteration1/src/simulator/Simulation.java
@@ -1,6 +1,5 @@
 package simulator;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +10,18 @@ import floor.SimulationEntry;
 import scheduler.Scheduler;
 
 public class Simulation {
+	
+	private static Thread floorSubsystemThread;
+	private static Thread elevatorSubsystemThread;
+	
+	public static Thread getFloorSubsystemThread() {
+		return floorSubsystemThread;
+	}
+
+	public static Thread getElevatorSubsystemThread() {
+		return elevatorSubsystemThread;
+	}
+
 	public static void main(String[] args) throws IOException {
 
 		// TODO Iteration 2 should read elevator number, floor number, and file input
@@ -25,11 +36,13 @@ public class Simulation {
 		scheduler.addElevatorSubsys(elevatorSubsystem);
 		scheduler.addFloorSubsys(floorSubsystem);
 
-		Thread floorSubsystemThread = new Thread(floorSubsystem);
-		Thread elevatorSubsystemThread = new Thread(elevatorSubsystem);
+		floorSubsystemThread = new Thread(floorSubsystem);
+		elevatorSubsystemThread = new Thread(elevatorSubsystem);
 
 		floorSubsystemThread.start();
 		elevatorSubsystemThread.start();
 
 	}
+	
+	
 }
