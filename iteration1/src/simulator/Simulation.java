3c49863a764487b551fd028e8d64cd4765e93864
Subear J <subearjama1@gmail.com>
2023-02-04 18:49:05 -0500

    Fixed elevator constructor in Simulation + removed sleep() + FloorSubsystemTest + README

--- iteration1/src/simulator/Simulation.java
+++ iteration1/src/simulator/Simulation.java
@@ -5,6 +5,10 @@ import elevator.ElevatorSubsystem;
 import floor.FloorSubsystem;
 import scheduler.Scheduler;
 
+/**
+ * Simulation class is responsible for program initialization & runs the simulation from the console
+ * @author Will Forest
+ */
 public class Simulation {
 	
 	private static Thread floorSubsystemThread;
@@ -19,23 +23,24 @@ public class Simulation {
 	}
 
 	public static void main(String[] args) throws IOException {
-
 		// TODO Iteration 2 should read elevator number, floor number, and file input from command line
-		String testFile = "test/resources/request_test.txt";
-
+		
+		String testFile = "test/resources/request_test.txt"; // directory of test file for FloorSubsystem
+		int maxFloors = 7; // the maximum amount of floors in the FloorSubsystem and ElevatorSubsystem
+		
+		// Create objects for Scheduler, ElevatorSubsystem, and FloorSubsystem.
 		Scheduler scheduler = new Scheduler();
-		ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(scheduler, 1);
-		FloorSubsystem floorSubsystem = new FloorSubsystem(testFile,scheduler, 7);
-
+		ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(scheduler, 1, maxFloors, 1);
+		FloorSubsystem floorSubsystem = new FloorSubsystem(testFile,scheduler, maxFloors);
 		scheduler.addElevatorSubsys(elevatorSubsystem);
 		scheduler.addFloorSubsys(floorSubsystem);
-
+		
+		// Create threads passing them their respective objects 
 		floorSubsystemThread = new Thread(floorSubsystem);
 		elevatorSubsystemThread = new Thread(elevatorSubsystem);
-
+		
+		// Start to activate all run() methods for each thread
 		floorSubsystemThread.start();
 		elevatorSubsystemThread.start();
-
 	}
-	
 }
