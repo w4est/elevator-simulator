94b93fbb1ad5d9e04168167349562a56f12a0fa9
FMahamud <91301445+FMahamud@users.noreply.github.com>
2023-03-10 19:39:02 -0500

    Removed commented out tests

--- iteration2+/elevator-module/src/test/java/elevator/ElevatorSubsystemTest.java
+++ iteration2+/elevator-module/src/test/java/elevator/ElevatorSubsystemTest.java
@@ -25,63 +25,6 @@ import common.Request;
 
 public class ElevatorSubsystemTest {
 
-	// FIXME
-	/*
-	 * @Test void shouldPollSchedulerWhenIdle() {
-	 * 
-	 * Scheduler schedulerMock = Mockito.mock(Scheduler.class);
-	 * 
-	 * // On the first attempt, say that the scheduler is not done // On the second,
-	 * inform the elevatorThread to shutdown
-	 * when(schedulerMock.isDone()).thenReturn(false, true);
-	 * 
-	 * Thread elevatorThread = new Thread(new ElevatorSubsystem(schedulerMock, 1));
-	 * elevatorThread.start();
-	 * 
-	 * assertTrue(elevatorThread.isAlive()); // Give the thread some time to
-	 * interact try { Thread.sleep(100); } catch (InterruptedException e) {
-	 * e.printStackTrace(); } // Verify that the elevator subsystem checked with the
-	 * scheduler verify(schedulerMock, atLeastOnce()).elevatorNeeded();
-	 * 
-	 * // Wait for the elevator to finish try { elevatorThread.join(500); } catch
-	 * (InterruptedException e) { e.printStackTrace(); }
-	 * 
-	 * assertFalse(elevatorThread.isAlive()); }
-	 */
-
-	/**
-	 * This test case goes through the states of an elevator from the perspective of
-	 * the ElevatorSubsystem.
-	 * 
-	 * It tests how the ElevatorSubsystem handles multiple requests at the same time
-	 * 
-	 * NOTE: another way to test states is to run simulation and look at the console
-	 * 
-	 * @author Subear Jama
-	 */
-	// FIXME
-	/*
-	 * @Test void shouldTestElevatorStates() { //Initialization Scheduler
-	 * schedulerMock = Mockito.mock(Scheduler.class); ElevatorSubsystem
-	 * elevatorSubsystem = new ElevatorSubsystem(schedulerMock, 1); // Before
-	 * starting thread, add scheduler requests manually into ElevatorSubsystem
-	 * Request r1 = new Request(2, Direction.UP, 5); Request r2 = new Request(4,
-	 * Direction.UP, 5); Request r3 = new Request(3, Direction.UP, 7);
-	 * elevatorSubsystem.updateFloorQueue(r1);
-	 * elevatorSubsystem.updateFloorQueue(r2);
-	 * elevatorSubsystem.updateFloorQueue(r3); Thread elevatorThread = new
-	 * Thread(elevatorSubsystem); elevatorThread.start();
-	 * 
-	 * // check print statements in console (added into ElevatorSubsystem to see
-	 * current floor & state) /*If you see this in the console before the
-	 * ElevatorSubsystem finishes, Ignore. Doesn't happen in actual simulation:
-	 * 
-	 * ElevatorSubsystem: Start Operate Cycle 1. Elevator Current Floor & State:
-	 * Floor 1, State: STOP_OPENED 2. Elevator Current Floor & State: Floor 1,
-	 * State: STOP_CLOSED
-	 */
-	// }
-
 	@Test
 	@SuppressWarnings("rawtypes")
 	void receiveRequest() {
